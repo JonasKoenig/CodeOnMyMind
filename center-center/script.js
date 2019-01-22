d12b9fd393ad487d9571babde82ea7d84e1bb984
JonasKoenig <jonas-koenig@gmx.net>
2019-01-22 19:33:59 +0100

    Center-center: Modulo bug fixed

--- center-center/script.js
+++ center-center/script.js
@@ -5,18 +5,23 @@ var start = new Date().getTime();
 var elapsed = 0;
 var timerOffset = 0;
 
+var minutes, seconds, mm, ss = 0;
+
+
 setInterval(function() {
   elapsed = (new Date().getTime() - start)/1000;
 
   seconds = Math.floor(elapsed)%60;
-  ss = seconds<10 ? "0"+seconds : seconds;
   minutes = Math.floor(elapsed/60)%60;
+
+  ss = seconds<10 ? "0"+seconds : seconds;
   mm = minutes<10 ? "0"+minutes : minutes;
   clock.innerHTML = mm + ":" + ss;
 
-  seconds -= Math.floor(timerOffset)%60;
+  seconds = Math.floor(elapsed - timerOffset)%60;
+  minutes = Math.floor(elapsed/60 - timerOffset/60)%60;
+
   ss = seconds<10 ? "0"+seconds : seconds;
-  minutes -= Math.floor(timerOffset/60)%60;
   mm = minutes<10 ? "0"+minutes : minutes;
   timer.innerHTML = mm + ":" + ss;
 
